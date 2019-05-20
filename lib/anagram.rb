df0f6b96332345cf98c711f5325e53819fb83ccb
johncban <johncban@gmail.com>
2019-05-20 02:23:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,5 +5,9 @@ class Anagram
   def initialize(say_word)
     @say_word = say_word
   end
-  
+
+  def matchit(word_arrays)
+    word_arrays.select do |el|
+      (@word_arrays.split("").sort) == (el.split("").sort)
+  end
 end

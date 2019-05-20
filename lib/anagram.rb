80003fe15a1385f9d039d17e44c8f826fc9d625c
johncban <johncban@gmail.com>
2019-05-20 02:26:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,12 +2,12 @@
 class Anagram
   attr_accessor :say_word
 
-  def initialize(say_word)
-    @say_word = say_word
+  def initialize(word)
+    @word = word
   end
 
   def matchit(word_arrays)
-    word_arrays.select do |el|
+    word.select do |el|
       (@word_arrays.split("").sort) == (el.split("").sort)
     end
   end
